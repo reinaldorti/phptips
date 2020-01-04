d1d5a44ac9b0e98307dfbe13a84f72f0792a91ff
Reinaldo Dorti <reinaldorti@gmail.com>
2020-01-04 09:53:27 -0300

    finalizando ep10

--- ep10/medias.php
+++ ep10/medias.php
@@ -2,24 +2,28 @@
 
 require __DIR__ . "/vendor/autoload.php";
 
-$upload = new \CoffeeCode\Uploader\File("storege", "files");
+$upload = new \CoffeeCode\Uploader\Media("storege", "files");
 $files = $_FILES;
 
 if (!empty($files["file"])) {
     $file = $files["file"];
 
     if (empty($file["type"]) || !in_array($file["type"], $upload::isAllowed())) {
-        echo "<p>Selecione um arquivo válido!</p>";
+        echo "<p>Selecione um mídia válida!</p>";
     } else {
         $uploaded = $upload->upload($file, pathinfo($file["name"], PATHINFO_FILENAME), 350);
         echo "<a target='_blank' href='{$uploaded}'>Acessar Arquivo</a>";
     }
 }
 
+$sended = filter_input(INPUT_GET, "sended", FILTER_VALIDATE_BOOLEAN);
+if ($sended && empty($files["file"])) {
+    echo "<p>Selecione uma imagme de até " . ini_get("upload_max_filesize") . "!</p>";
+}
 ?>
 
-<form action="" method="post" enctype="multipart/form-data">
-    <h1>Send File</h1>
+<form action="?sended=true" method="post" enctype="multipart/form-data">
+    <h1>Send Mídia</h1>
     <input type="file" name="file"/>
     <button>Enviar</button>
 </form>
