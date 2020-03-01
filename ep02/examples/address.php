29dd38ab22147b49d18d0768fe87db1b106cec5f
Reinaldo Dorti <reinaldorti@gmail.com>
2020-03-01 20:06:09 -0300

    fazendo a licao de casa

--- ep02/examples/address.php
+++ ep02/examples/address.php
@@ -4,11 +4,13 @@ require __DIR__ . "/../vendor/autoload.php";
 use Source\Models\User;
 use Source\Models\Address;
 
-//MINHA LIÇÃO DE CASA
-
+//LIÇÃO DE CASA
 $user = (new User())->findById(1);
+$user->first_name = "Reinaldo Azevedo";
+$user->last_name = "Dorti";
+$user->save();
 
-$addr = new Address();
-$addr->add($user, "Rua: Votuporanga", "55");
+$addr = (new Address())->findById(1);
+$addr->add($user, "Votuporanga", "99");
 
 $addr->save();
